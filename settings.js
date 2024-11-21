f338a397f253b1d06be52d357bc8a97fe537c2b9
krizz <154887919+krsna081@users.noreply.github.com>
2024-11-21 21:00:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'NdyZz'// yourname
-global.MONGO_DB_URI = ''
+global.creator = 'krsna_081'// yourname
+global.MONGO_DB_URI = 'mongodb+srv://krsna_081:JTYUBGk1HrhUQZpR@myapi.owerp.mongodb.net/?retryWrites=true&w=majority&appName=myapi'
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "sandikurniawan12042004@gmail.com"
-global.email_password = "plgeklttdfciejjw"
+global.your_email = "krizzioxyz@gmail.com"
+global.email_password = "uert ypnk kbtk gdlp"
 global.limitCount = 1000
 global.YUOR_PORT = 8000
 global.listApi = ['zpka_b772f4f8372b4ca6965371399918bb5f_444c635b','zpka_87fcc29824e446618f9f0d45f27653f1_3a6ce29d','zpka_136dab30f03945dd983bb8f5ee18215c_54fe3569','zpka_948d594e4cf2495fa9e9827524ea4732_3f973216']
